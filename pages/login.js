41f1831c77c8c2cdb543d467ee2fb6449a3cb56a
RidaZaidi05 <158581173+RidaZaidi05@users.noreply.github.com>
2024-12-22 19:51:12 +0500

    login

--- pages/login.js
+++ pages/login.js
@@ -17,7 +17,7 @@ const LoginPage = () => {
     // Check if the token exists on the login page
     const checkToken = async () => {
       try {
-        const response = await fetch('/api/getToken');
+        const response = await fetch('https://mm-black-phi.vercel.app/api/getToken');
         if (response.ok) {
           // If a valid token is found, redirect to the admin dashboard
           router.push('/admin-dashboard');
@@ -37,7 +37,7 @@ const LoginPage = () => {
     // Simulate login process (replace with actual authentication logic)
     setTimeout(async () => {
       try {
-        const response = await fetch('/api/login', {
+        const response = await fetch('https://mm-black-phi.vercel.app/api/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json', // Correct Content-Type
